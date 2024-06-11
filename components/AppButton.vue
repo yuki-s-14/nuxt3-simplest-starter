c1fd2d8b300b8ed024504ad8374159d66bf99de9
yuki-s-14 <30838251+yuki-s-14@users.noreply.github.com>
2024-06-12 00:18:43 +0900

    [add]ボタンコンポーネントとテストコードの追加

---
+++ components/AppButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  buttonText: string
+  alertMessage: string
+}>()
+const showAlert = (message: string) => {
+  alert(message)
+}
+</script>
+<template>
+  <button @click="showAlert(props.alertMessage)">{{ props.buttonText }}</button>
+</template>
