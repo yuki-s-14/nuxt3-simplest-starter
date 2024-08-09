5af1b1d431e7507ec64cc3ed33d2ec676dcc0ac7
yuki-s-14 <30838251+yuki-s-14@users.noreply.github.com>
2024-08-10 01:25:15 +0900

    [update]NuxtIslandコンポーネントとサーバーコンポーネントの追加

---
+++ components/AppTestServer.server.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import moment from 'moment'
+const date = moment().format('MMMM Do YYYY, h:mm:ss a')
+const props = defineProps<{
+  title: string
+  count: number
+}>()
+const doAlert = (title: string) => {
+  alert(title)
+}
+</script>
+<template>
+  <div>
+    <h1>{{ props.title }} - {{ props.count }}</h1>
+    <div>{{ date }}</div>
+    <button @click="doAlert(props.title)">アラート発出</button>
+  </div>
+</template>
