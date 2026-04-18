1a0192ab34f1a56e0bbd68717a0c8db5c6409e7e
yuki-s-14 <30838251+yuki-s-14@users.noreply.github.com>
2026-04-18 23:05:56 +0900

    [add]Fetch検証パターンの追加

---
+++ pages/fetch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Fetch Example</h1>
+    <img :src="fetchData.message" alt="Random Dog Image" >
+  </div>
+</template>
+<script setup lang="ts">
+
+// $fetchの場合
+const fetchData = await $fetch('https://dog.ceo/api/breeds/image/random')
+console.log('$fetch:', fetchData.message)
+
+// useFetchの場合
+// const { data: fetchData } = await useFetch('https://dog.ceo/api/breeds/image/random', {
+//   key: 'useFetchRandomDog',
+// })
+// console.log('useFetch:', fetchData.value?.message)
+
+// useAsyncDataの場合
+// const { data: fetchData } = await useAsyncData('useAsyncDataRandomDog', () => $fetch('https://dog.ceo/api/breeds/image/random'))
+// console.log('useAsyncData:', fetchData.value?.message)
+</script>
